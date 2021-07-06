6035d70c0507dcaccc8e5e3e7b7287d2aab38212
bcharlotin1 <biancacharlotin1@gmail.com>
2021-07-06 13:03:39 -0400

    commit tree c1a430c2759dde48fc9567c440284a845f1e33fe

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validate :non_clickbait
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    validates :category, inclusion: { in:["Fiction", "Non-Fiction"]}
+   
+
+    def non_clickbait
+      
+        if self.title
+            if self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top") || self.title.include?("Guess")
+            else errors.add(:title, "change the title")
+            end
+        end
+    end 
 end
+
+
